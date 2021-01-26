f3448fe881d6f57a223cb9c19f266228f1bbc664
Rok Hribar <a.rok.hribar@gmail.com>
2021-01-26 14:15:40 +0100

    :sparkles: Added product template.

--- projfrontend/src/core/ProductPage.js
+++ projfrontend/src/core/ProductPage.js
@@ -9,6 +9,9 @@ import {getProducts} from "./helper/coreapicalls";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Signup from "../user/Signup";
 import "../static/sass/main.css";
+import locationFonts from '../static/icons/font/flaticon.css';
+import prices from '../static/img/kronplatz-prices.png';
+import soon from '../static/img/coming-soon.png'
 
 const ProductPage = ({
     product,
@@ -50,33 +53,107 @@ const ProductPage = ({
             <Navigation/>
             
             <div class="product">
-                <img src = {product.image} class= "product__image" alt={`${product.name} mountain`} />
-                <div class="product__info">
-                    <div class="product__heading">
-                        <div class="product__heading--name">{product.name}</div>
-                        <div class="product__heading--location"><i class="fas fa-map-marker-alt"></i>{product.country}, {product.mountains}</div>
-                        <div class="product__heading--peak"><i class="fas fa-mountain"></i>{product.peak_altitude} m</div>
+                
+                <div class="product__header">
+                    {/* <span className={locationFonts['flaticon-location']}></span> */}
+                    <img src = {product.image} class= "product__image" alt={`${product.name} mountain`} />
+                    <div class="product__info">
+                        <div class="product__heading">
+                            <div class="product__heading--name">{product.name}</div>
+                        </div>
+                        <div class="product__basic">
+                            <div class="product__heading--location"><i class="flaticon-location"></i>{product.country}, {product.mountains}</div>
+                            <div class="product__heading--peak"><i class="flaticon-mountain"></i>{product.peak_altitude} m</div>
+                            <div class="product__heading--load"><i class="flaticon-attachment"></i>Download your map!</div>
+                            
+                        </div>
+                        <div class="product__slopes">
+                            <div class="product__slopes--easy">Easy – {product.slopes_easy} km</div>
+                            <div class="product__slopes--medium">Medium –   {product.slopes_medium} km</div>
+                            <div class="product__slopes--hard">Hard –   {product.slopes_hard} km</div>
+                            <div class="product__slopes--total">ALL –  {product.slopes_total} km</div>
+                        </div>
+                        <div className="product__title">RATINGS</div>
+                        <div class="product__ratings">
+                            <div class="product__ratings--resort"><div class="product__ratings--rating">{product.rating_resort}</div>RESORT</div>
+                            <div class="product__ratings--family"><div class="product__ratings--rating">{product.rating_family}</div>FAMILY FRIENDLY</div>
+                            <div class="product__ratings--scenery"><div class="product__ratings--rating">{product.rating_scenery}</div>SCENERY</div>
+                        </div>
+                        <div class="product__text">{product.description_long}</div>
+                        <div className="product__buy">
+                            <div class="product__price">{product.price} EUR</div>
+                            {/* <div class="product__snow">
+                                <div class="product__snow--mountain">{product.snow_mountain}</div>
+                                <div class="product__snow--valley">{product.snow_valley}</div>
+                            </div> */}
+                            <div className="product__buttons">
+                                <div className="signin__form__button">
+                                    <button className="button navigation__buttons--blue button--page product__button--cart">Add To cart</button>
+                                </div>
+                                <div className="signin__form__button">
+                                    <button className="button navigation__buttons--blue button--page product__button--buy">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="product__ratings">
-                        <div class="product__ratings--resort">Resort Rating {product.rating_resort}</div>
-                        <div class="product__ratings--family">Family Friendly Rating {product.rating_family}</div>
-                        <div class="product__ratings--scenery">Scenery Rating {product.rating_scenery}</div>
-                    </div>
-                    <div class="product__slopes">
-                        <div class="product__slopes--easy"><i class="fas fa-circle"></i>{product.slopes_easy} km</div>
-                        <div class="product__slopes--medium"><i class="fas fa-circle"></i>{product.slopes_medium} km</div>
-                        <div class="product__slopes--hard"><i class="fas fa-circle"></i>{product.slopes_hard} km</div>
-                        <div class="product__slopes--total">ALL {product.slopes_total} km</div>
-                    </div>
-                    <div class="product__snow">
-                        <div class="product__snow--mountain">{product.snow_mountain}</div>
-                        <div class="product__snow--valley">{product.snow_valley}</div>
-                    </div>
-                    <div class="product__description">{product.description_long}</div>
-                    <div class="product__price">{product.price}</div>
                 </div>
+                
+                <div className="product__prices">
+                    <h2 className="product__prices--heading">prices per season</h2>
+                    <img src={prices} alt="Seasonal prices" className="product__prices--table"></img>
+                </div>
+
+                <div className="product__description">
+                    <h2 className="product__description--heading">description</h2>
+                    <div className="product__description--block">
+                        <div className="product__description--title">Accessbility</div>
+                        <div className="product__description--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </div>
+                    </div>
+                    <div className="product__description--block">
+                        <div className="product__description--title">Food / Drink</div>
+                        <div className="product__description--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </div>
+                    </div>
+                    <div className="product__description--block">
+                        <div className="product__description--title">Offpiste</div>
+                        <div className="product__description--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </div>
+                    </div>                     
+                </div>
+
+                <div className="product__prices">
+                    <h2 className="product__prices--heading">weather</h2>
+                    
+                </div>
+
+                <div className="product__description">
+                    <h2 className="product__description--heading">cameras</h2>
+                    <div className="product__description--block">
+                        <img src={soon} alt="Cameras" className="product__description--soon"></img>
+                        <img src={soon} alt="Cameras" className="product__description--soon"></img>
+                        <img src={soon} alt="Cameras" className="product__description--soon"></img>
+                    </div>
+                    
+                </div>
+
+                <div className="product__prices">
+                    <h2 className="product__prices--heading">community</h2>
+                    <p>Add a comment about your experience!</p>
+                    <p>This feature will be enabled soon!</p>
+                </div>
+
             </div>
-            
+
+
+
+
             
             {products.map(product1 =>
                     <Route exact path={`/${product1.name}`} component={Signup} />
