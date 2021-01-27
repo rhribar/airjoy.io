e5539cdb0f4bd1079457baabf4aefe14113b4997
Rok Hribar <a.rok.hribar@gmail.com>
2021-01-27 19:51:36 +0100

    hopefully fixed error

--- ecom/settings.py
+++ ecom/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
+django_heroku.settings(locals())
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
