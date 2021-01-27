81daf99bf8f17ab6b6338a6629e45b30a0818025
Rok Hribar <a.rok.hribar@gmail.com>
2021-01-27 02:21:48 +0100

    :rocket: Setting up for deployment.

--- ecom/settings.py
+++ ecom/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
