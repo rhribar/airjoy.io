36c650cc51640c821948abad997ac465863e67bc
Rok Hribar <a.rok.hribar@gmail.com>
2021-01-30 20:17:20 +0100

    set up aws s3 bucket for media files

--- ecom/settings.py
+++ ecom/settings.py
@@ -197,6 +197,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+
 '''
 AWS_ACCESS_KEY_ID = '*****************'
 AWS_SECRET_ACCESS_KEY = '*****************'
